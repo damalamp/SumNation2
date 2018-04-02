30c3ad48f1d03ce6cc35afe4a095fa36b80fa37e
damalamp <david.m.lamp@gmail.com>
2018-04-01 23:26:42 -0700

    BigLayout Improvements

--- app/src/main/java/com/xindere/davidlamp/sumnation2/MainActivity.java
+++ app/src/main/java/com/xindere/davidlamp/sumnation2/MainActivity.java
@@ -15,56 +15,56 @@ import android.os.Handler;
 public class MainActivity extends AppCompatActivity {
     double goalTotal, currentTotal;
     Random rand = new Random();
-    Chronometer stopWatch;
-    Handler handler;
     private int seconds=0;
-
+    boolean running;
+    TextView txtTimerView,goalView,currentTotalView;
+    long startTime=0L,timeInMilliseconds=0L;
+    Handler handler = new Handler();
+    Runnable updateTimerThread = new Runnable(){
+        @Override
+        public void run(){
+            timeInMilliseconds = SystemClock.uptimeMillis()-startTime;
+            int seconds = (int)(timeInMilliseconds/1000);
+            int milliseconds = (int)(timeInMilliseconds%1000);
+            txtTimerView.setText(String.format("%2d",seconds)+"."+String.format("%03d",milliseconds));
+            handler.postDelayed(this,0);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        stopWatch = findViewById(R.id.stopWatch);
         goalTotal = 0;
-        TextView goalView = findViewById(R.id.goal_total);
-        goalView.setText(String.valueOf(goalTotal));
-    }
+        txtTimerView = findViewById(R.id.timerValue);
+        currentTotalView = findViewById(R.id.current_total);
+        goalView = findViewById(R.id.goal_total);
+        }
 
     public void addValue(View v) {
-        int buttonNumber = Integer.parseInt(v.getTag().toString());
-        currentTotal = currentTotal + Math.pow(2, buttonNumber);
-        TextView currentTotalView = findViewById(R.id.current_total);
-        currentTotalView.setText(String.valueOf((int) (currentTotal)));
-        if (currentTotal > goalTotal) {
-            currentTotalView.setBackgroundColor(Color.RED);
+        if (currentTotal < goalTotal) {
+            int buttonNumber = Integer.parseInt(v.getTag().toString());
+            currentTotal = currentTotal + Math.pow(2, buttonNumber);
+            currentTotalView.setText(String.valueOf((int) (currentTotal)));
+            if (currentTotal > goalTotal) {
+                currentTotalView.setBackgroundColor(Color.RED);
+                handler.removeCallbacks(updateTimerThread);
+            }
+            if (currentTotal == goalTotal) {
+                currentTotalView.setBackgroundColor(Color.GREEN);
+                running = false;
+                handler.removeCallbacks(updateTimerThread);
+            }
         }
-        if (currentTotal == goalTotal) {
-            currentTotalView.setBackgroundColor(Color.GREEN);
-        }
-
     }
 
     public void resetGame(View v) {
         currentTotal = 0;
-        TextView currentTotalView = findViewById(R.id.current_total);
         currentTotalView.setBackgroundColor(Color.TRANSPARENT);
         currentTotalView.setText(String.valueOf((int) (currentTotal)));
         goalTotal = rand.nextInt(2047) + 1;
-        TextView goalView = findViewById(R.id.goal_total);
         goalView.setText(String.valueOf(goalTotal));
-        //stopWatch.setBase(SystemClock.elapsedRealtime());
-        //stopWatch.start();
-        handler = new Handler();
-        handler.post(new Runnable(){
-            @Override
-            public void run() {
-                int secs = seconds;
-                //int milliseconds = ;
-                String time = String.format("%02d.%03d", seconds, seconds);
-                handler.postDelayed(this, 0);
-            }
-
-        });
-
+        startTime = SystemClock.uptimeMillis();
+        handler.postDelayed(updateTimerThread,0);
     }
 }
